fe7bc2355ec5717efad12a7da33d753cddf73cb9
korupi <corrupt@kou.st>
2025-09-23 23:09:20 +0200

    chore(package): bump version, also in fact i did have an idea how to optimize

--- src/audio.rs
+++ src/audio.rs
@@ -1,109 +1,97 @@
+//! Audio playback thing
+///
+/// This module provides a simple asynchronous audio player
+///
+/// # Example
+/// ```ignore
+/// let mut audio = Audio::new();
+/// audio.play_audio(Path::new("sound.wav"), false).await?;
+/// ```
 use std::{
-    collections::HashMap,
-    fs,
-    io::{BufReader, Cursor},
+    io::BufReader,
     path::{Path, PathBuf},
-    thread,
 };
 
-use anyhow::bail;
 use tokio::sync::mpsc::{self, Sender};
 
+/// Holds the command channel
 #[derive(Debug, Clone)]
 pub struct Audio {
+    /// Tokio mpsc sender used to communicate with the audio thread
     pub channel: Sender<AudioCommand>,
-    pub cached_audio: HashMap<String, Box<Cursor<Vec<u8>>>>,
 }
 
+/// Commands that can be sent to the audio thread.
 #[derive(Debug, Clone)]
 pub enum AudioCommand {
+    /// Play the file at the given path
     Play(PathBuf),
+    /// Stop the current playback
     Stop,
 }
 
 impl Audio {
+    /// Creates a new [`Audio`] instance and spawns the background audio thread
+    ///
+    /// Workflow:
+    /// 1. Opens a default `rodio` output stream.
+    /// 2. Listens on an mpsc channel for `AudioCommand`s.
+    /// 3. On `Play`, stops any existing sink and starts a new one.
     pub fn new() -> Audio {
         let (tx, mut rx) = mpsc::channel::<AudioCommand>(16);
 
         tokio::spawn(async move {
-            println!("hey im the audio thread nya meow meow >:3");
+            println!("Audio thread started");
             let stream_handle = rodio::OutputStreamBuilder::open_default_stream()
                 .expect("audio: failed to initialize stream handle");
             let mixer = stream_handle.mixer();
             let mut current_sink: Option<rodio::Sink> = None;
 
             while let Some(message) = rx.recv().await {
-                println!("{:?}", message);
-                if let AudioCommand::Play(pathbuf) = message {
-                    if let Some(sink) = current_sink {
-                        sink.stop();
-                    }
+                match message {
+                    AudioCommand::Play(pathbuf) => {
+                        if let Some(sink) = current_sink.take() {
+                            sink.stop();
+                        }
 
-                    let file = std::fs::File::open(pathbuf.to_string_lossy().to_string()).unwrap();
-                    println!("{:?}", file.metadata());
-                    let sink = rodio::play(mixer, BufReader::new(file)).unwrap();
-                    sink.set_volume(1.0);
-                    current_sink = Some(sink);
+                        let file = std::fs::File::open(&pathbuf)
+                            .expect("audio: failed to open file");
+                        let sink = rodio::play(mixer, BufReader::new(file))
+                            .expect("audio: failed to start playback");
+                        sink.set_volume(1.0);
+                        current_sink = Some(sink);
+                    }
+                    AudioCommand::Stop => {
+                        if let Some(sink) = current_sink.take() {
+                            sink.stop();
+                        }
+                    }
                 }
-                thread::sleep(std::time::Duration::from_millis(100));
             }
         });
 
         Self {
-            channel: tx,
-            cached_audio: HashMap::new(),
+            channel: tx
         }
     }
 
-    pub fn load_asset(&mut self, path: &Path) -> anyhow::Result<()> {
-        if self
-            .cached_audio
-            .contains_key(&path.to_string_lossy().to_string())
-        {
-            bail!("resource already cached")
-        }
-
-        let data: Vec<u8> = fs::read(path)?;
-        self.cached_audio.insert(
-            path.to_string_lossy().to_string(),
-            Box::new(Cursor::new(data)),
-        );
-        Ok(())
-    }
-
+    /// Sends a `Play` command for the given path
     pub async fn play_audio(
         &mut self,
         path: &Path,
         _interrupt_current_playback: bool,
     ) -> anyhow::Result<()> {
-        if !self
-            .cached_audio
-            .contains_key(&path.to_string_lossy().to_string())
-        {
-            // as we do not want to crash just because the resource wasn't cached we'll cache it
-            // here and now
-            println!("caching");
-            self.load_asset(path)?;
-        }
+        // TODO: use [`ResourceManager`]
         self.channel
             .send(AudioCommand::Play(path.to_path_buf()))
             .await?;
-        /*
-        // .unwrap() here should be safe because we cached the resource if it wasn't already cached
-        // before
-        let resource: Box<Cursor<Vec<u8>>> = self.cached_audio.get(&path.to_string_lossy().to_string()).unwrap().clone();
-
-        if !self.playing.contains(&path.to_string_lossy().to_string()) {
-            println!("playing");
-            self.playing.push(path.to_string_lossy().to_string());
-            rodio::play(&self.mixer, resource)?;
-        }
-        */
         Ok(())
     }
 }
 
+// clippy said i need `Default`
 impl Default for Audio {
+    /// Shortcut for `Audio::new()`
     fn default() -> Self {
         Self::new()
     }
