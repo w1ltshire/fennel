453b2fad00ce7d364b5f6a2bf6abc09a7cc919ac
korupi <corrupt@kou.st>
2025-09-24 15:21:09 +0200

    tests(graphics,resources): add tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,21 +1,45 @@
 #[cfg(test)]
-use std::path::PathBuf;
 
-#[test]
-fn resource_management() {
-    use crate::resources::{LoadableResource, ResourceManager, loadable};
+#[tokio::test]
+async fn window_creation() {
+    use std::process::exit;
 
-    let mut manager = ResourceManager::new();
-    let path = PathBuf::from("examples/example.png");
-    let asset = loadable::Image::load(path.clone());
+    use crate::{EventHandler, Game, events, graphics};
+    struct TestGameState {}
 
-    manager.cache_asset(asset.unwrap()).unwrap();
-    assert_eq!(
-        "examples/example.png",
-        manager
-            .resources
-            .get(&path.to_string_lossy().to_string())
-            .unwrap()
-            .name()
+    #[async_trait::async_trait]
+    impl EventHandler for TestGameState {
+        async fn update(&self, _game: &mut Game) -> anyhow::Result<()> {
+            Ok(())
+        }
+
+        async fn draw(&self, game: &mut Game) -> anyhow::Result<()> {
+            use sdl3::pixels::Color;
+            game.graphics.canvas.set_draw_color(Color::RGB(0, 0, 0));
+            game.graphics.canvas.clear();
+            game.graphics
+                .draw_image(
+                    "examples/example.png".to_string(),
+                    (0.0, 0.0),
+                    &mut game.resource_manager,
+                )
+                .expect("failed to draw an image");
+            game.graphics.canvas.present();
+            assert!(game.resource_manager.is_cached("examples/example.png".to_string()));
+            exit(0);
+        }
+    }
+
+    static INIT: std::sync::Once = std::sync::Once::new();
+    INIT.call_once(|| {
+        unsafe { std::env::set_var("SDL_VIDEODRIVER", "dummy") };
+    });
+
+    let graphics = graphics::Graphics::new(String::from("my cool game"), (500, 500));
+    let mut game = Game::new(
+        String::from("my cool game"),
+        String::from("wiltshire"),
+        graphics.unwrap(),
     );
+    events::run(&mut game, Box::new(TestGameState {})).await;
 }
