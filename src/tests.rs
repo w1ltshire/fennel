9d226a613fd81022d65ddd8dcb7468ed13a88f68
korupi <corrupt@kou.st>
2025-09-26 12:27:49 +0200

    tests: improve

--- src/tests.rs
+++ src/tests.rs
@@ -1,34 +1,9 @@
 #[cfg(test)]
 
 #[tokio::test]
-async fn window_creation() {
-    use std::process::exit;
-
-    use crate::{EventHandler, Game, events, graphics};
-    struct TestGameState {}
-
-    #[async_trait::async_trait]
-    impl EventHandler for TestGameState {
-        async fn update(&self, _game: &mut Game) -> anyhow::Result<()> {
-            Ok(())
-        }
-
-        async fn draw(&self, game: &mut Game) -> anyhow::Result<()> {
-            use sdl3::pixels::Color;
-            game.graphics.canvas.set_draw_color(Color::RGB(0, 0, 0));
-            game.graphics.canvas.clear();
-            game.graphics
-                .draw_image(
-                    "examples/example.png".to_string(),
-                    (0.0, 0.0),
-                    &mut game.resource_manager,
-                )
-                .expect("failed to draw an image");
-            game.graphics.canvas.present();
-            assert!(game.resource_manager.is_cached("examples/example.png".to_string()));
-            exit(0);
-        }
-    }
+async fn font_load() {
+    use std::path::PathBuf;
+    use crate::{Game, graphics, resources::loadable, resources::LoadableResource};
 
     static INIT: std::sync::Once = std::sync::Once::new();
     INIT.call_once(|| {
@@ -41,5 +16,55 @@ async fn window_creation() {
         String::from("wiltshire"),
         graphics.unwrap(),
     );
-    events::run(&mut game, Box::new(TestGameState {})).await;
+
+    let asset = loadable::Font::load(PathBuf::from("examples/terminus.ttf"), &mut game.graphics, Some(16.0)).expect("failed to load font");
+    game.resource_manager.cache_asset(asset).unwrap();
+    game.resource_manager.resources.iter().for_each(|e| println!("{}", e.0));
+
+    let cached_asset = game.resource_manager.get_asset(String::from("Terminus (TTF) 16"));
+
+    // check is:
+    // 1. asset cached successfully
+    // 2. name generated successfully
+    assert!(cached_asset.is_ok());
+
+    let font: &loadable::Font = crate::resources::as_concrete(cached_asset.unwrap()).unwrap();
+    assert_eq!(font.size, 16.0);
+}
+
+#[tokio::test]
+async fn image_load() {
+    use std::path::PathBuf;
+    use crate::{Game, graphics, resources::loadable, resources::LoadableResource};
+
+    static INIT: std::sync::Once = std::sync::Once::new();
+    INIT.call_once(|| {
+        unsafe { std::env::set_var("SDL_VIDEODRIVER", "dummy") };
+    });
+
+    let graphics = graphics::Graphics::new(String::from("my cool game"), (500, 500));
+    let mut game = Game::new(
+        String::from("my cool game"),
+        String::from("wiltshire"),
+        graphics.unwrap(),
+    );
+
+    let asset = loadable::Image::load(PathBuf::from("examples/example.png"), &mut game.graphics, None).expect("failed to load image");
+    game.resource_manager.cache_asset(asset).unwrap();
+    game.resource_manager.resources.iter().for_each(|e| println!("{}", e.0));
+
+    let cached_asset = game.resource_manager.get_asset(String::from("examples/example.png"));
+
+    // check is:
+    // 1. asset cached successfully
+    // 2. name generated successfully
+    assert!(cached_asset.is_ok());
+
+    let image: &loadable::Image = crate::resources::as_concrete(cached_asset.unwrap()).unwrap();
+
+    // the example image dimensions is 128 x 128, unless some fuck replaces it this will pass
+    assert_eq!(image.width, 128);
+    assert_eq!(image.height, 128);
+    assert_eq!(image.width, image.texture.width());
+    assert_eq!(image.height, image.texture.height());
 }
