349e325496cb6502b77774a4eafe8f2e4810a21e
w1ltshire <wiltshire@ruf.st>
2025-11-24 21:52:02 +0200

    feat(examples): switch to a scene, move the camera

--- crates/fennel-engine/examples/game.rs
+++ crates/fennel-engine/examples/game.rs
@@ -1,21 +1,22 @@
 use sdl3::keyboard::Scancode;
 use fennel_engine::app::AppBuilder;
 use specs::{Join, System, WriteExpect, WriteStorage};
+use fennel_engine::camera::Camera;
 use fennel_engine::ecs::sprite::Sprite;
 use fennel_engine::events::KeyEvents;
+use fennel_engine::scenes::ActiveScene;
 
 struct SysA;
 
 impl<'a> System<'a> for SysA {
-    type SystemData = (WriteExpect<'a, KeyEvents>, WriteStorage<'a, Sprite>);
+    type SystemData = (WriteExpect<'a, KeyEvents>, WriteStorage<'a, Sprite>, WriteExpect<'a, ActiveScene>, WriteExpect<'a, Camera>);
 
-    fn run(&mut self, (mut key_events, mut sprite): Self::SystemData) {
+    fn run(&mut self, (mut key_events, mut sprite, mut active_scene, mut camera): Self::SystemData) {
         // std::thread::sleep(std::time::Duration::from_millis(183)); // artificial delay, aim for ~5.5 tps
         // println!("nya");
         for sprite in (&mut sprite).join() {
             key_events.0.drain(..).for_each(|event| {
                 if let Some(scancode) = event.scancode {
-                    println!("{:?}", event);
                     match scancode {
                         Scancode::W => {
                             sprite.transform.position.1 -= 4.0;
@@ -29,6 +30,22 @@ impl<'a> System<'a> for SysA {
                         Scancode::D => {
                             sprite.transform.position.0 += 4.0;
                         },
+                        Scancode::Space => {
+                            active_scene.name = String::from("test");
+                            active_scene.loaded = false;
+                        },
+                        Scancode::Up => {
+                            camera.position.1 += 1.0;
+                        },
+                        Scancode::Down => {
+                            camera.position.1 -= 1.0;
+                        },
+                        Scancode::Left => {
+                            camera.position.0 += 1.0;
+                        },
+                        Scancode::Right => {
+                            camera.position.0 -= 1.0;
+                        }
                         _ => {}
                     }
                 }
